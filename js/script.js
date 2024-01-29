5b35dd0ec07520909b3ca184a50c68ecb4c16408
L1nar <122251664+L1nar@users.noreply.github.com>
2024-01-29 17:25:41 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ const input = document.querySelector('#input');
 const list = document.querySelector('#list');
 
 btn.addEventListener('click', (event) => {
-    const newItem = document.createElement('li'); // document.createElement(); - adds element.
+    let newItem = document.createElement('li'); // document.createElement(); - adds element.
     newItem.classList.add('item'); //element.classList - adds or removes classes for an element.
     newItem.textContent = input.value; //textContent - allows you to read or set the text content of an element.
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'x';
     deleteBtn.classList.add('delete');
     deleteBtn.addEventListener('click', () => {
-        list.removeChild(newItem); // parent.removeChild - removes the specified element from parent.
+      const conf = confirm('Вы действительно хотите удалить эту задачу?');
+            if (conf === true) {
+                list.removeChild(newItem); // parent.removeChild - removes the specified element from parent.
+            } else {
+                return false;
+            }     
     })
     newItem.appendChild(deleteBtn); // parent.appendChild - allows you to insert any element at the end of the element.
     list.appendChild(newItem);
@@ -18,4 +23,9 @@ btn.addEventListener('click', (event) => {
     newItem.addEventListener('click', (event) => {
         event.target.classList.toggle('checked'); // classList.toggle(); - добовляет класс на страницу, если его нет и удаляет, если он есть.
     });
-})
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        btn.click();
+      }
+    })
+});
